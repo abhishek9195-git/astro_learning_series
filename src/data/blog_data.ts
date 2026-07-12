3fa5a66d0b7f0ca0b4b3a742e129368c53deb689
abhishek <abhishek.km2010@gmail.com>
2026-07-12 09:11:47 +0530

    updates

---
+++ src/data/blog_data.ts
@@ -0,0 +1,14 @@
+export const BLOG_DATA = [
+  {
+    title: "Astro Basics",
+    slug: "astro-basics"
+  },
+  {
+    title: "Astro Routing",
+    slug: "astro-routing"
+  },
+  {
+    title: "Astro Components",
+    slug: "astro-components"
+  }
+];
